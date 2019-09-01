d6633a8ca61ccbfdaba345f04c64f2731b4ce25d
Greg-tts <gtesto@techtalentsouth.com>
2019-08-31 19:37:42 -0700

    Updates

---
+++ precache-manifest.3380c6cfa704fa4c783815ad1567e309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ffc0d8ba2ff267963c4d06125715e0",
+    "url": "/Greg-tts/simonUnleashed/index.html"
+  },
+  {
+    "revision": "56aca08fb4d239384d09",
+    "url": "/Greg-tts/simonUnleashed/static/css/main.fed096e4.chunk.css"
+  },
+  {
+    "revision": "ac1029731b95a6d6c80c",
+    "url": "/Greg-tts/simonUnleashed/static/js/2.e167017e.chunk.js"
+  },
+  {
+    "revision": "56aca08fb4d239384d09",
+    "url": "/Greg-tts/simonUnleashed/static/js/main.8e838149.chunk.js"
+  },
+  {
+    "revision": "42e14d76b153d23968ea",
+    "url": "/Greg-tts/simonUnleashed/static/js/runtime~main.6a336390.js"
+  }
+]);
